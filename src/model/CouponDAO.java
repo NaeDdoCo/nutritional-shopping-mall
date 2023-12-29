e9f645f954b5ee73fa4c6706e4d3ab4c5f5760fd
정훈석 <wjdgnstjr93@gmail.com>
2023-12-29 15:33:35 +0900

    [update] SQL문 및 메서드 변경 selectone : 쿠폰번호 확인 -> 쿠폰번호 확인 및 할인율 선택 update : 쿠폰번호가 같다면 USED를 FALSE로 변경

--- src/model/CouponDAO.java
+++ src/model/CouponDAO.java
@@ -16,11 +16,12 @@ public class CouponDAO {
 
 	// RAll, 쿠폰 목록 출력
 	// 출력대상 : 쿠폰이름 쿠폰번호 쿠폰할인율 쿠폰유효기간 쿠폰사용 가능한 카테고리
-	public static final String selectAll = "SELECT CPID, CPNAME, PERIOD, DISCOUNT, CATEGORY " + "FROM COUPON";
+	public static final String selectAll = "SELECT CPID, CPNAME, PERIOD, DISCOUNT, CATEGORY FROM COUPON";
 
 	// ROne, 쿠폰번호 확인
-	// 선택한 쿠폰이 있는지 확인
-	public static final String selectOne = "SELECT CPID FROM COUPON WHERE CPID = ?";
+	// 선택한 쿠폰번호와 같은 행의 쿠폰번호와 할인율을 가져온다
+	// MID(쿠폰보유회원)도 같이 가져와서 유효성 검사에 사용
+	public static final String selectOne = "SELECT CPID, DISCOUNT, MID FROM COUPON WHERE CPID = ?";
 
 	// C, 쿠폰 추가 및 부여
 	// 생성해서 부여할 때 생성해야 하는 컬럼
@@ -31,9 +32,9 @@ public class CouponDAO {
 			+ "VALUES (?, ?, ?, ?, ?, ?)";
 
 	// U, 쿠폰정보변경
-	// 사용하면 쿠폰에 적용상품과 사용여부 갱신
+	// 사용하면 쿠폰에 사용여부 갱신
 	// 조건은 입력한 쿠폰번호
-	public static final String update = "UPDATE COUPON SET PID = ?, USE = ? WHERE CPID = ?";
+	public static final String update = "UPDATE COUPON SET USED = FALSE WHERE CPID = ?";
 
 	// D1, 제품 삭제(회수?)
 	// 문제있는 쿠폰을 삭제
@@ -43,8 +44,6 @@ public class CouponDAO {
 	// 쿠폰을 잘못주면 회수
 	private static final String deleteMID = "DELETE FROM COUPON WHERE MID = ?";
 
-	
-	
 	public ArrayList<CouponDTO> selectAll() {
 		// DB에 연결해서 COUPON테이블에 있는 데이터를 불러온다
 		// 불러온 데이터를 DTO객체에 넣어준다
@@ -104,12 +103,12 @@ public class CouponDAO {
 
 			pstmt.setString(1, CPID);
 
-			// SQL문 실행결과 저장
 			ResultSet rs = pstmt.executeQuery();
 
-			// 받아온 행의 속성을 객체에 저장
 			if (rs.next()) {
 				couponDTO.setCPID(rs.getString("CPID"));
+				couponDTO.setDiscount(rs.getInt("DISCOUNT"));
+				couponDTO.setMID(rs.getString("MID"));
 			}
 
 			rs.close();
@@ -123,41 +122,27 @@ public class CouponDAO {
 		return couponDTO;
 	}
 
-	private void insert(CouponDTO couponDTO) {
-
+	private boolean insert(CouponDTO couponDTO) {
+		return true;
 	}
 
-
-	public int update(CouponDTO couponDTO) {
+	public boolean update(CouponDTO couponDTO) {
 		// selectOne에서 값이 있다면
-		// 입력한 쿠폰번호를 가진 행의 적용제품과 사용여부를 변경
-		
+		// 입력한 쿠폰번호를 가진 행의 사용여부를 변경
+
 		// SQL의 결과를 반환할 변수
 		int result = 0;
-		
+
 		// 입력한 제품의 쿠폰번호
 		String CPID = couponDTO.getCPID();
-
-		// 사용하는 제품의 제품번호
-		int PID = couponDTO.getPID();
-		
-		// 변경하려는 유통기한
-		// boolean타입의 변수는 get을 생성할때 자동으로 is로 작성된다
-		boolean use = couponDTO.isUse();
-
-		// 변경하려는 데이터를 한 객체에 저장
-		CouponDTO cDTO = new CouponDTO();
-		cDTO.setPID(PID);
-		cDTO.setUse(use);
 
 		// DB에 연결
 		conn = JDBCUtil.connect();
 
 		try {
+			// 사용하면 USED == FALSE
 			pstmt = conn.prepareStatement(update);
-			pstmt.setInt(1, PID);
-			pstmt.setBoolean(2, use);
-			pstmt.setString(3, CPID);
+			pstmt.setString(1, CPID);
 
 			result = pstmt.executeUpdate();
 
@@ -167,11 +152,14 @@ public class CouponDAO {
 			// DB연결 종료
 			JDBCUtil.disconnection(pstmt, conn);
 		}
-		
-		return result;
+
+		if (result > 0) {	// result가 1이상이면 성공
+			return true;
+		}
+		return false;	// 아니면 실패
 	}
 
-	private void delete(CouponDTO couponDTO) {
-
+	private boolean delete(CouponDTO couponDTO) {
+		return true;
 	}
 }
