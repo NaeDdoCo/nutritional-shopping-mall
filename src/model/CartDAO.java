7407caf9348dd2d851a3625a4bff7e9aaf684064
jjeong623 <hajeonghui@gmail.com>
2023-12-31 05:11:07 +0900

    [UPDATE] CartDAO selectAll 매개변수수정, 전체 매개변수명 변경(ctrl참고), 주석 수정

--- src/model/CartDAO.java
+++ src/model/CartDAO.java
@@ -10,13 +10,12 @@ public class CartDAO {
 	private Connection conn; //DB와의 연결을 담당
 	private PreparedStatement pstmt; // CRUD 수행을 담당
 
-	private static final String SELECTALL="SELECT CID,PID,MID,CNT FROM CART";
-	private static final String SELECTONE="SELECT (CID,MID,CNT) FROM CART WHERE PID=?";
-																			//'문자열'
-	private static final String INSERT="INSERT INTO CART VALUES((SELECT NVL(MAX(CID),0) + 1 FROM CART),'?',?,?)";
-	private static final String UPDATE="UPDATE CART SET CNT = ? WHERE PID = ?";
-	private static final String DELETE="DELETE FROM CART WHERE MID = ?"; //카트 전체 비우기
-	private static final String DELETE_PID="DELETE FROM CART WHERE PID= ?"; //부분 삭제
+	static final String SELECTALL="SELECT CID,PID,MID,CNT FROM CART";
+	static final String SELECTONE="SELECT (CID,MID,CNT) FROM CART WHERE PID=?";
+	static final String INSERT="INSERT INTO CART VALUES((SELECT NVL(MAX(CID),0) + 1 FROM CART),'?',?,?)";
+	static final String UPDATE="UPDATE CART SET CNT = ? WHERE PID = ?";
+	static final String DELETE="DELETE FROM CART WHERE MID = ?"; //카트 전체 비우기
+	private static final String DELETE_PID="DELETE FROM CART WHERE MID =? AND PID= ?"; //부분 삭제
 
 
 	/*
@@ -28,189 +27,21 @@ public class CartDAO {
 
 	장바구니에 같은 상품이 담겨있지 않다면 insert
 	같은 상품이 담겨있다면 update
-	--------------------------------------------------------------------
-	1. 장바구니 추가
-	public boolean insert (CartDTO cDTO){ //성공 여부 반환 
-		매개변수 : CartDTO 모두
-	(cnt가 음수 또는 0)라면
-		-> false 반환
-	conn 연결
-    pstmt연결
-    INSERT쿼리문 실행
-		-> cart DB에 추가 CartDTO 모두 저장
-		RS에 결과값 담기
-
-		conn,pstmt 해제
-			RS값이 있다면
-			true 반환;
-
-			아니면 false반환;
-
-			선택한 상품의 cnt가 실재고보다 작다면, 담지 못함
-	    return = null; //insert 실패
-
-	2. 
-	public CartDTO selectOne(CartDTO cDTO){ //1개 조회
-			매개변수 : PID
-		반환해줄 CartDTO data 초기화 
-		//CartDTO data=null;
-
-		conn연결
-		pstmt연결
-		SELECTONE 쿼리 실행
-		PID 중복검사 -> 일치(장바구니에 있는 상품이면)
-		data에 저장
-
-		conn,pstmt 해제
-
-		data 반환; //저장된 값이 없다면 초기값 null이 반환됨
-	 }   
-
-	 3.
-	 public boolean update(CartDTO cDTO){ //성공 여부 반환
-	 	매개변수 : PID,CNT
-	 	conn연결
-	 	pstmt연결
-	 	UPDATE 쿼리문 실행
-	 	RS생성
-	 	장바구니의 해당 pid의 cnt 추가해주기
-	 	RS해제?
-	 	결과값이 없다면 {
-	 	false 반환;
-	 	}
-	 	true 반환;
-	 }
-
-	 -------------------------------------------------
-	장바구니 목록보기R(2개이상 조회) selectall
-	담긴 제품 정보 보여주기
-	-> 반환 cartList(CID,MID,PID,CNT)
-
-
-	4.
-	public ArrayList<CartDTO> selectAll(CartDTO cDTO){ 
-			매개변수 : MID
-		반환해 줄 ArrayList<CartDTO> cartList 생성 -> !null -> 아래 쿼리에서 rs를 담던 안담던 계속 null이 아닌게 된다!
-
-		conn 연결
-		pstmt연결
-
-		SELECTAll쿼리문 실행
-
-		결과값 resultSet에 담기
-
-			rs결과값이 있다면{
-			cartDTO data 생성
-			data에 CART테이블에서 가져온 정보 저장(CID,MID,PID,CNT)
-
-			catch 
-			return null;
-
-			cartList에 data에 담긴 정보 추가
-		}
-		사용이 끝난 rs 해제
-		conn,pstmt해제
-
-
-		카트리스트가 비었다면 null, 비어있지 않다면 cartList 반환 //에러면 null, 빈배열
-		//return cartList.isEmpty() ? null : cartList; ->제거
-}		
-
-	5.장바구니 비우기
-	public boolean delete(CartDTO cDTO) { //성공 여부 반환
-							매개변수 mid->일치하는것 삭제
-	conn연결
-	pstmt연결
-
-		delete 쿼리문실행
-		장바구니db 데이터 삭제실행 
-		그결과값을 result에 담기
-		result가 0이면(삭제가 이루어지지 않았다면)
-		false반환;
-
-	conn해제
-	pstmt해제
-
-	return true;
-	}
-
 	 */
-	
 
-	public  boolean delete(CartDTO cDTO) {
-//		매개변수 : MID로 일치하는 데이터 삭제
-		conn=JDBCUtil.connect();
-		try {
-			pstmt=conn.prepareStatement(DELETE);
-			pstmt.setString(1, cDTO.getMid());
-			int result = pstmt.executeUpdate();//영향을 받은 숫자 저장
-			if(result<=0) {
-				return false;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}finally {
-			JDBCUtil.disconnection(pstmt, conn);
-		}return true;
-	}
 
-	
-	public boolean insert (CartDTO cDTO){
-		if(cDTO.getCnt()<=0) {
-			return false;
-		}
-		conn=JDBCUtil.connect();
-		try {
-			pstmt=conn.prepareStatement(INSERT); //MID,PID,CNT
-			pstmt.setString(1, cDTO.getMid());
-			pstmt.setInt(2, cDTO.getPid());
-			pstmt.setInt(3, cDTO.getCnt());
-			int result = pstmt.executeUpdate();//영향을 받은 숫자 저장
-			if(result<=0) {
-				return false;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}finally {
-			JDBCUtil.disconnection(pstmt, conn);
-		}
-		return true;
-	}
-
-//	public ArrayList<CartDTO> selectAll(){ 
-//		반환해 줄 ArrayList<CartDTO> cartList 생성 -> !null -> 아래 쿼리에서 rs를 담던 안담던 계속 null이 아닌게 된다!
-//
-//		conn 연결
-//		pstmt연결
-//
-//		SELECTAll쿼리문 실행
-//
-//		결과값 resultSet에 담기
-//
-//			rs결과값이 있다면{
-//			cartDTO data 생성
-//			data에 CART테이블에서 가져온 정보 저장(CID,MID,PID,CNT)
-//
-//			catch 
-//			return null;
-//
-//			cartList에 data에 담긴 정보 추가
-//		}
-//		사용이 끝난 rs 해제
-//		conn,pstmt해제
-//	return cartlist;
-	
-	public ArrayList<CartDTO> selectAll(){
+	//장바구니 목록 출력
+	public ArrayList<CartDTO> selectAll(CartDTO cartDTO){
+		//반환해 줄 cdatas 생성
 		ArrayList<CartDTO> cdatas = new ArrayList<CartDTO>(); //!null
-		
-//		매개변수 : xxxx
-		conn=JDBCUtil.connect();
+
+		conn=JDBCUtil.connect(); //DB연결
 		try {
 			pstmt=conn.prepareStatement(SELECTALL);
-//			private static final String SELECTALL="SELECT CID,PID,MID,CNT FROM CART";
-			ResultSet rs=pstmt.executeQuery();
+//			static final String SELECTALL="SELECT CID,PID,MID,CNT FROM CART";
+			ResultSet rs=pstmt.executeQuery(); //쿼리 실행 후 결과로 나온 데이터를 rs에 담기
 
-			while(rs.next()) {
+			while(rs.next()) { //결과 데이터가 있을때 까지 반복->rs의 결과값을 data에 담아 cdatas에 저장
 				CartDTO data=new CartDTO();
 				data.setCid(rs.getInt("CID"));
 				data.setMid(rs.getString("MID"));
@@ -218,94 +49,109 @@ public class CartDAO {
 				data.setCnt(rs.getInt("CNT"));
 				cdatas.add(data);
 			}
-			rs.close();
+			rs.close(); //ResultSet 해제
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
 		}finally {
-			JDBCUtil.disconnection(pstmt, conn);
+			JDBCUtil.disconnection(pstmt, conn); //필수조건 : DB연결해제
 		}
 		return cdatas;
 	}
 
-//	 public boolean update(CartDTO cDTO){ //성공 여부 반환
-//	 	매개변수 : PID,CNT
-//	 	conn연결
-//	 	pstmt연결
-//	 	UPDATE 쿼리문 실행
-//	 	RS생성
-//	 	장바구니의 해당 pid의 cnt 추가해주기
-//	 	RS해제?
-//	 	결과값이 없다면 {
-//	 	false 반환;
-//	 	}
-//	 	true 반환;
-//	 }
-	public boolean update(CartDTO cDTO){
-//	 	매개변수 : PID,CNT
-		conn=JDBCUtil.connect();
-		try {
-			pstmt=conn.prepareStatement(UPDATE);
-//			private static final String UPDATE="UPDATE CART SET CNT = ? WHERE PID = ?";
-			pstmt.setInt(1, cDTO.getCnt());
-			pstmt.setInt(2, cDTO.getPid());
-			int result = pstmt.executeUpdate();//영향을 받은 숫자 저장
-			if(result<=0) {
-				return false;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}finally {
-			JDBCUtil.disconnection(pstmt, conn);
-		}
-		return true;
-	}
-
-//	public CartDTO selectOne(CartDTO cDTO){ //1개 조회
-//			매개변수 : PID
-//		반환해줄 CartDTO data 초기화 
-//		//CartDTO data=null;
-//
-//		conn연결
-//		pstmt연결
-//		SELECTONE 쿼리 실행
-//		PID 중복검사 -> 일치(장바구니에 있는 상품이면)
-//		data에 저장
-//
-//		conn,pstmt 해제
-//
-//		data 반환; //저장된 값이 없다면 초기값 null이 반환됨
-//	 }   
 	public CartDTO selectOne(CartDTO cDTO){ 
-//		매개변수 : PID
+		//      PID를 입력받아 존재여부 확인?? -> 현재 미사용
 		CartDTO data = null;
-		conn=JDBCUtil.connect();
+		conn=JDBCUtil.connect();//DB연결
 		try {
 			pstmt=conn.prepareStatement(SELECTONE);
-//			private static final String SELECTONE="SELECT (CID,MID,CNT) FROM CART WHERE PID=?";
+//			static final String SELECTONE="SELECT (CID,MID,CNT) FROM CART WHERE PID=?";
 			pstmt.setInt(1, cDTO.getPid());
-			ResultSet rs=pstmt.executeQuery();
+			ResultSet rs=pstmt.executeQuery(); //입력된 PID에 해당하는 결과 데이터를 rs 저장
 
-			while(rs.next()) {
+			while(rs.next()) { //PID가 일치한다면 data에 저장되어 반환 ,아니라면 초기값 null 반환
 				data=new CartDTO();
 				data.setCid(rs.getInt("CID"));
 				data.setMid(rs.getString("MID"));
 				data.setCnt(rs.getInt("CNT"));
 			}
-			rs.close();
+			rs.close();//ResultSet 해제
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
 		}finally {
-			JDBCUtil.disconnection(pstmt, conn);
+			JDBCUtil.disconnection(pstmt, conn); //필수조건 : DB연결해제
 		}
-		
-		
 		return data;
 	}
-	
 
-	
-	
+	//장바구니 추가
+	public boolean insert (CartDTO cartDTO){
+		if(cartDTO.getCnt()<=0) { //입력할 cnt가 0 또는 음수라면
+			return false;			//insert 실패
+		}
+		conn=JDBCUtil.connect(); //DB연결
+		try {
+			pstmt=conn.prepareStatement(INSERT);
+//			static final String INSERT="INSERT INTO CART VALUES((SELECT NVL(MAX(CID),0) + 1 FROM CART),'?',?,?)";
+			pstmt.setString(1, cartDTO.getMid());
+			pstmt.setInt(2, cartDTO.getPid());
+			System.out.println(cartDTO.getCnt());
+			pstmt.setInt(3, cartDTO.getCnt()); 
+			int result = pstmt.executeUpdate(); //추가된 행의 수 저장
+			if(result<=0) { 	//추가된 행의 수가 없다면
+				return false;	//INSERT 실패 반환
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			JDBCUtil.disconnection(pstmt, conn); //필수조건 : DB연결해제
+		}
+		return true; //INSERT 성공 반환
+	}
+
+	//장바구니에 같은 제품이 있다면 수량만 변경
+	public boolean update(CartDTO cartDTO){
+		//	    해당하는 PID의 CNT를 추가
+		conn=JDBCUtil.connect(); //DB연결
+		try {
+			pstmt=conn.prepareStatement(UPDATE);
+//			static final String UPDATE="UPDATE CART SET CNT = ? WHERE PID = ?";
+			pstmt.setInt(1, cartDTO.getCnt());
+			pstmt.setInt(2, cartDTO.getPid());
+			int result = pstmt.executeUpdate(); //변경된 행의 수 저장
+			if(result<=0) { //변경된 행의 수가 없다면 
+				return false; //UPDATE 실패 반환
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			JDBCUtil.disconnection(pstmt, conn); //필수조건 : DB연결해제
+		}
+		return true; //UPDATE 성공 반환
+	}
+
+	//장바구니 비우기
+	public  boolean delete(CartDTO cartDTO) {
+		//		MID로 일치하는 데이터 삭제
+		conn=JDBCUtil.connect(); //DB연결
+		try {
+			pstmt=conn.prepareStatement(DELETE);
+//			static final String DELETE="DELETE FROM CART WHERE MID = ?";
+			pstmt.setString(1, cartDTO.getMid());
+			int result = pstmt.executeUpdate(); //삭제된 행의 수 저장
+			if(result<=0) {  //삭제가 이루어지지 않았다면
+				return false;  //DELETE 실패 반환
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			JDBCUtil.disconnection(pstmt, conn); //필수조건 : DB해제
+		}
+		return true; //DELETE 성공 반환
+	}
+
+
 }
+
 
